7b997e497395b77815f3ef635e1614f0069be69d
Simone Zabberoni <simone.zabberoni@gmail.com>
2018-05-05 10:17:34 +0200

    Added automatic inventory mass enable

---
+++ hostEnableInventory.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Enable automatic inventory on all existing hosts.
+You can also filter with a dict:
+
+    f  = {  'host' : 'somehostname'  }
+    hosts = zapi.host.get(output=['hostids', 'host'], filter=f)
+
+You can filter by name, template, group etc...
+
+"""
+
+from zabbix.api import ZabbixAPI
+
+zabbixServer    = 'http://yousrserver/zabbix/'
+zabbixUser      = 'someuser'
+zabbixPass      = 'somepass'
+
+zapi = ZabbixAPI(url=zabbixServer, user=zabbixUser, password=zabbixPass)
+
+hosts = zapi.host.get(output=['hostids', 'host'])
+
+for host in hosts:
+    # Enable automatic inventory
+    res = zapi.host.update(hostid=host['hostid'], inventory_mode=1)
+    print res
