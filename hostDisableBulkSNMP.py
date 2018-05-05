6949b49742c359dd6d729fc8051e03ff1602f1b8
Simone Zabberoni <simone.zabberoni@gmail.com>
2018-05-05 10:38:00 +0200

    Add SNMP bulk disabler

---
+++ hostDisableBulkSNMP.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Disable bulk SNMP on all existing hosts.
+You can also filter with a dict:
+
+    f  = {  'host' : 'somehostname'  }
+    hosts = zapi.host.get(output=['hostids', 'host'], filter=f)
+
+You can filter by name, template, group etc...
+
+"""
+
+from zabbix.api import ZabbixAPI
+
+zabbixServer    = 'http://yousrserver/zabbix/'
+zabbixUser      = 'someuser'
+zabbixPass      = 'somepass'
+
+zapi = ZabbixAPI(url=zabbixServer, user=zabbixUser, password=zabbixPass)
+
+interfaces = zapi.hostinterface.get()
+
+for interface in interfaces:
+    if (interface['type'] == '2' ):
+        print "Hostid: {} InterfaceID {} ".format(interface['hostid'], interface['interfaceid'])
+        zapi.hostinterface.update(interfaceid=interface['interfaceid'], bulk='0')
