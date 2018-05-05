48e6c74e490bd9e4a34a0b4d55586c071d24e3e5
Simone Zabberoni <simone.zabberoni@gmail.com>
2018-05-05 10:12:50 +0200

    Added check inventory name

---
+++ checkInventoryName.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Check all hosts and return host name <-> inventory name matching
+"""
+
+from zabbix.api import ZabbixAPI
+
+zabbixServer    = 'http://yousrserver/zabbix/'
+zabbixUser      = 'someuser'
+zabbixPass      = 'somepass'
+
+
+zapi = ZabbixAPI(url=zabbixServer, user=zabbixUser, password=zabbixPass)
+
+hosts = zapi.host.get(selectInventory='extend', output=['hostids', 'host'])
+
+for host in hosts:
+    if (host['inventory']['name']):
+        if (host['inventory']['name'] is not host['host']):
+            print "Mismatch on id {}: Host Name: {} - SNMP Inventory Name: {} ".format(host['hostid'], host['host'],  host['inventory']['name'])
+        else:
+            print "Matching inventory name on id {}: Host Name: {}".format(host['hostid'], host['host'],  host['inventory']['name'])
+    else:
+         print "Missing inventory name on id {}: Host Name: {}".format(host['hostid'], host['host'],  host['inventory']['name'])
+
+
