38eed20b546e6e3cd0081e5afc8f9d832ae1ffb3
Simone Zabberoni <simone.zabberoni@gmail.com>
2018-06-27 16:52:31 +0200

    getItemHistory and getItemTrend scripts

---
+++ getItemHistoryByName.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Get history values for specific items in a time range:
+
+# ./getItemHistoryByName.py -H some-host  -I "ICMP response time" -f "26/6/2018 16:00" -t "27/6/2018 23:59" 
+ItemID: 77013 - Item: ICMP response time - Key: icmppingsec
+1530021641 26/06/2018 16:00:41 Value: 0.1042
+1530021701 26/06/2018 16:01:41 Value: 0.0993
+1530021762 26/06/2018 16:02:42 Value: 0.1024
+1530021822 26/06/2018 16:03:42 Value: 0.0966
+[cut]
+
+"""
+
+from zabbix.api import ZabbixAPI
+import sys, argparse
+import time
+import datetime
+
+
+zabbixServer    = 'http://yousrserver/zabbix/'
+zabbixUser      = 'someuser'
+zabbixPass      = 'somepass'
+
+
+
+def main(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-H', required=True, metavar='Hostname')
+    parser.add_argument('-I', required=True, metavar='Item Name')
+    parser.add_argument('-f', required=True, metavar='From Timestamp')
+    parser.add_argument('-t', required=True, metavar='Till Timestamp')
+
+    args = parser.parse_args()
+
+
+    zapi = ZabbixAPI(url=zabbixServer, user=zabbixUser, password=zabbixPass)
+
+    fromTimestamp = time.mktime(datetime.datetime.strptime(args.f, "%d/%m/%Y %H:%M").timetuple())
+    tillTimestamp = time.mktime(datetime.datetime.strptime(args.t, "%d/%m/%Y %H:%M").timetuple())
+
+
+    f  = {  'name' : args.I  }
+    items = zapi.item.get(filter=f, host=args.H, output='extend' )
+
+    for item in items:
+        print "ItemID: {} - Item: {} - Key: {}".format(item['itemid'], item['name'], item['key_'])
+        
+        values = zapi.trend.get(itemids=item['itemid'], time_from=fromTimestamp, time_till=tillTimestamp, history=item['value_type'])
+
+        for historyValue in values:
+            currentDate = datetime.datetime.fromtimestamp(int(historyValue['clock'])).strftime('%d/%m/%Y %H:%M:%S')
+
+            print "{} {} Value: Min: {} - Max: {} - Avg: {}".format(historyValue['clock'], currentDate, historyValue['value_min'], historyValue['value_max'], historyValue['value_avg'],)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
+
---
+++ getItemTrendByName.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Get trend values (min/max/avg) for specific items in a time range:
+
+# ./getItemTrendByName.py -H some-host  -I "ICMP Loss" -f "26/6/2018 00:00" -t "27/6/2018 23:59"
+ItemID: 77012 - Item: ICMP loss - Key: icmppingloss
+1529964000 26/06/2018 00:00:00 Value: Min: 0.0000 - Max: 100.0000 - Avg: 6.6667
+1529967600 26/06/2018 01:00:00 Value: Min: 0.0000 - Max: 0.0000 - Avg: 0.0000
+1529971200 26/06/2018 02:00:00 Value: Min: 0.0000 - Max: 0.0000 - Avg: 0.0000
+[cut]
+
+"""
+
+from zabbix.api import ZabbixAPI
+import sys, argparse
+import time
+import datetime
+
+
+zabbixServer    = 'http://yousrserver/zabbix/'
+zabbixUser      = 'someuser'
+zabbixPass      = 'somepass'
+
+
+def main(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-H', required=True, metavar='Hostname')
+    parser.add_argument('-I', required=True, metavar='Item Name')
+    parser.add_argument('-f', required=True, metavar='From Timestamp')
+    parser.add_argument('-t', required=True, metavar='Till Timestamp')
+
+    args = parser.parse_args()
+
+
+    zapi = ZabbixAPI(url=zabbixServer, user=zabbixUser, password=zabbixPass)
+
+    fromTimestamp = time.mktime(datetime.datetime.strptime(args.f, "%d/%m/%Y %H:%M").timetuple())
+    tillTimestamp = time.mktime(datetime.datetime.strptime(args.t, "%d/%m/%Y %H:%M").timetuple())
+
+
+    f  = {  'name' : args.I  }
+    items = zapi.item.get(filter=f, host=args.H, output='extend' )
+
+    for item in items:
+        print "ItemID: {} - Item: {} - Key: {}".format(item['itemid'], item['name'], item['key_'])
+        
+        values = zapi.history.get(itemids=item['itemid'], time_from=fromTimestamp, time_till=tillTimestamp, history=item['value_type'])
+
+        for historyValue in values:
+            currentDate = datetime.datetime.fromtimestamp(int(historyValue['clock'])).strftime('%d/%m/%Y %H:%M:%S')
+
+            print "{} {} Value: {}".format(historyValue['clock'], currentDate, historyValue['value'])
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
+
