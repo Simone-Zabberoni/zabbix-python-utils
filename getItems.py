232885dbc1b662ba72743762a7c08d78f59cb4d4
Simone Zabberoni <simone.zabberoni@gmail.com>
2018-06-27 16:38:28 +0200

    add getItems.py script

---
+++ getItems.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Get item names for a specific host
+
+# ./getItems.py some-host | grep ICMP
+HostID: 12556 - Host: some-host - ItemID: 267223 - Item: ICMP ping - Key: icmpping
+HostID: 12556 - Host: some-host - ItemID: 267224 - Item: ICMP loss - Key: icmppingloss
+HostID: 12556 - Host: some-host - ItemID: 267225 - Item: ICMP response time - Key: icmppingsec
+"""
+
+from zabbix.api import ZabbixAPI
+import sys
+
+zabbixServer    = 'http://yousrserver/zabbix/'
+zabbixUser      = 'someuser'
+zabbixPass      = 'somepass'
+
+zapi = ZabbixAPI(url=zabbixServer, user=zabbixUser, password=zabbixPass)
+
+if (sys.argv[1:]):
+    # Filter based on the cmdline argument
+    f  = {  'host' : sys.argv[1:]  }
+    hosts = zapi.host.get(filter=f, output=['hostids', 'host'] )
+
+    for host in hosts:
+        items = zapi.item.get(hostids=host['hostid'])
+        for item in items:
+            print "HostID: {} - Host: {} - ItemID: {} - Item: {} - Key: {}".format(host['hostid'], host['host'], item['itemid'], item['name'], item['key_'])
+else:
+    print "Provide an hostname"
