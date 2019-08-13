38e4e6dd42317f23cf23993cf8b598cfd7a14fcd
Simone Zabberoni <simone.zabberoni@gmail.com>
2019-08-13 12:17:06 +0200

    Command line args with env var support, better print

--- getItemHistoryByName.py
+++ getItemHistoryByName.py
@@ -2,57 +2,169 @@
 # -*- coding: utf-8 -*-
 
 """
-Get trend values (min/max/avg) for specific items in a time range:
+# export ZABBIX_SERVER='https://your_zabbix_host/zabbix/'
+# export ZABBIX_PASSWORD='admin'
+# export ZABBIX_USERNAME='secretPassword'
 
-# ./getItemTrendByName.py -H some-host  -I "ICMP Loss" -f "26/6/2018 00:00" -t "27/6/2018 23:59"
-ItemID: 77012 - Item: ICMP loss - Key: icmppingloss
-1529964000 26/06/2018 00:00:00 Value: Min: 0.0000 - Max: 100.0000 - Avg: 6.6667
-1529967600 26/06/2018 01:00:00 Value: Min: 0.0000 - Max: 0.0000 - Avg: 0.0000
-1529971200 26/06/2018 02:00:00 Value: Min: 0.0000 - Max: 0.0000 - Avg: 0.0000
-[cut]
+# ./getItemHistoryByName.py -H SomeHost -I "ICMP Loss" -f "13/8/2019 00:00" -t "13/8/2019 23:59" | head
+Connecting to https://your_zabbix_host/zabbix/'
+Host SomeHost (192.168.203.19):
+ItemID: 158495 - Item: ICMP loss - Key: icmppingloss
+1565676016 13/08/2019 08:00:16 Value: 0.0000
+1565676076 13/08/2019 08:01:16 Value: 0.0000
+1565676136 13/08/2019 08:02:16 Value: 0.0000
+1565676196 13/08/2019 08:03:16 Value: 0.0000
+1565676256 13/08/2019 08:04:16 Value: 0.0000
+1565676316 13/08/2019 08:05:16 Value: 0.0000
 
 """
 
 from zabbix.api import ZabbixAPI
-import sys, argparse
+import sys
+import argparse
 import time
 import datetime
+import os
+import json
 
 
-zabbixServer    = 'http://yourserver/zabbix/'
-zabbixUser      = 'someuser'
-zabbixPass      = 'somepass'
+# Class for argparse env variable support
+class EnvDefault(argparse.Action):
+    # From https://stackoverflow.com/questions/10551117/
+    def __init__(self, envvar, required=True, default=None, **kwargs):
+        if not default and envvar:
+            if envvar in os.environ:
+                default = os.environ[envvar]
+        if required and default:
+            required = False
+        super(EnvDefault, self).__init__(default=default, required=required,
+                                         **kwargs)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        setattr(namespace, self.dest, values)
+
+
+def jsonPrint(jsonUgly):
+    print(json.dumps(jsonUgly, indent=4, separators=(',', ': ')))
+
+
+def ArgumentParser():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-Z',
+                        required=True,
+                        action=EnvDefault,
+                        envvar='ZABBIX_SERVER',
+                        help="Specify the zabbix server URL ie: http://yourserver/zabbix/ (ZABBIX_SERVER environment variable)",
+                        metavar='zabbix-server-url')
+
+    parser.add_argument('-u',
+                        required=True,
+                        action=EnvDefault,
+                        envvar='ZABBIX_USERNAME',
+                        help="Specify the zabbix username (ZABBIX_USERNAME environment variable)",
+                        metavar='Username')
+
+    parser.add_argument('-p',
+                        required=True,
+                        action=EnvDefault,
+                        envvar='ZABBIX_PASSWORD',
+                        help="Specify the zabbix username (ZABBIX_PASSWORD environment variable)",
+                        metavar='Password')
+
+    parser.add_argument('-f',
+                        required=True,
+                        help="Specify the \"from\" date - format 26/6/2018 00:00",
+                        metavar='from-date')
+
+    parser.add_argument('-t',
+                        required=True,
+                        help="Specify the \"to\" date - format 26/6/2018 00:00",
+                        metavar='to-date')
+
+    parser.add_argument('-H',
+                        required=True,
+                        help="Specify the hostname of the monitored device",
+                        metavar='hostname')
+
+    parser.add_argument('-I',
+                        required=True,
+                        help="Specify the item name",
+                        metavar='to-date')
+
+    return parser.parse_args()
 
 
 def main(argv):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-H', required=True, metavar='Hostname')
-    parser.add_argument('-I', required=True, metavar='Item Name')
-    parser.add_argument('-f', required=True, metavar='From Timestamp')
-    parser.add_argument('-t', required=True, metavar='Till Timestamp')
 
-    args = parser.parse_args()
+    # Parse arguments and build work variables
+    args = ArgumentParser()
+    zabbixURL = args.Z
+    zabbixUsername = args.u
+    zabbixPassword = args.p
+    itemName = args.I
+    hostName = args.H
 
+    hostFilter = {'name': hostName}
+    itemFilter = {'name': itemName}
 
-    zapi = ZabbixAPI(url=zabbixServer, user=zabbixUser, password=zabbixPass)
+    # Convert from/to dates into timestamps
+    fromTimestamp = time.mktime(datetime.datetime.strptime(
+        args.f, "%d/%m/%Y %H:%M").timetuple())
+    tillTimestamp = time.mktime(datetime.datetime.strptime(
+        args.t, "%d/%m/%Y %H:%M").timetuple())
 
-    fromTimestamp = time.mktime(datetime.datetime.strptime(args.f, "%d/%m/%Y %H:%M").timetuple())
-    tillTimestamp = time.mktime(datetime.datetime.strptime(args.t, "%d/%m/%Y %H:%M").timetuple())
+    # API Connect
+    print('Connecting to {}'.format(zabbixURL))
+    try:
+        zapi = ZabbixAPI(url=zabbixURL, user=zabbixUsername,
+                         password=zabbixPassword)
+    except Exception as e:
+        print("Connection error: {}".format(str(e)))
+        exit()
 
+    # Search the hostname
+    try:
+        hosts = zapi.host.get(filter=hostFilter, output=[
+                              'hostids', 'host', 'name'])
+    except Exception as e:
+        print("Host get error:{}".format(str(e)))
+        exit()
 
-    f  = {  'name' : args.I  }
-    items = zapi.item.get(filter=f, host=args.H, output='extend' )
+    if (len(hosts) == 0):
+        print("Can't find host {}".format(hostName))
+        exit()
 
+    # Search the item object
+    try:
+        items = zapi.item.get(filter=itemFilter, host=hosts[0]['host'],
+                              output='extend', selectHosts=['host', 'name'])
+    except Exception as e:
+        print("Get item error: {}".format(str(e)))
+        exit()
+
+    print("Host {} ({}):".format(hosts[0]['name'], hosts[0]['host']))
+    # for loop - future fuzzy search
     for item in items:
-        print "ItemID: {} - Item: {} - Key: {}".format(item['itemid'], item['name'], item['key_'])
-        
-        values = zapi.history.get(itemids=item['itemid'], time_from=fromTimestamp, time_till=tillTimestamp, history=item['value_type'])
+        print(
+            "ItemID: {} - Item: {} - Key: {}".format(item['itemid'], item['name'], item['key_']))
 
+        # Search values
+        try:
+            values = zapi.history.get(
+                itemids=item['itemid'], time_from=fromTimestamp, time_till=tillTimestamp, history=item['value_type'])
+        except Exception as e:
+            print("Values get error: {}".format(str(e)))
+            continue
+
+        # Convert date/timestamp and print values
         for historyValue in values:
-            currentDate = datetime.datetime.fromtimestamp(int(historyValue['clock'])).strftime('%d/%m/%Y %H:%M:%S')
+            currentDate = datetime.datetime.fromtimestamp(
+                int(historyValue['clock'])).strftime('%d/%m/%Y %H:%M:%S')
 
-            print "{} {} Value: {}".format(historyValue['clock'], currentDate, historyValue['value'])
+            print("{} {} Value: {}".format(
+                historyValue['clock'], currentDate, historyValue['value']))
+
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
-
+    main(sys.argv[1:])
--- getItemTrendByName.py
+++ getItemTrendByName.py
@@ -2,59 +2,169 @@
 # -*- coding: utf-8 -*-
 
 """
-Get history values for specific items in a time range:
 
-# ./getItemHistoryByName.py -H some-host  -I "ICMP response time" -f "26/6/2018 16:00" -t "27/6/2018 23:59" 
-ItemID: 77013 - Item: ICMP response time - Key: icmppingsec
-1530021641 26/06/2018 16:00:41 Value: 0.1042
-1530021701 26/06/2018 16:01:41 Value: 0.0993
-1530021762 26/06/2018 16:02:42 Value: 0.1024
-1530021822 26/06/2018 16:03:42 Value: 0.0966
-[cut]
+# export ZABBIX_SERVER='https://your_zabbix_host/zabbix/'
+# export ZABBIX_PASSWORD='admin'
+# export ZABBIX_USERNAME='secretPassword'
+
+# ./getItemTrendByName.py  -I 'Page count (BW)' -f "1/2/2019 00:00" -t "28/2/2019 18:00" -H SomePrinter
+Connecting to https://your_zabbix_host/zabbix/'
+Host SomePrinter (192.168.203.19):
+ItemID: 113312 - Item: Page count (BW) - Key: printer.pgBW
+1549029600 01/02/2019 15:00:00 Value: Min: 15523 - Max: 15523 - Avg: 15523
+1549033200 01/02/2019 16:00:00 Value: Min: 15523 - Max: 15531 - Avg: 15525
+1549036800 01/02/2019 17:00:00 Value: Min: 15531 - Max: 15531 - Avg: 15531
+1549263600 04/02/2019 08:00:00 Value: Min: 15532 - Max: 15532 - Avg: 15532
+1549267200 04/02/2019 09:00:00 Value: Min: 15538 - Max: 15538 - Avg: 15538
 
 """
 
 from zabbix.api import ZabbixAPI
-import sys, argparse
+import sys
+import argparse
 import time
 import datetime
+import os
+import json
 
 
-zabbixServer    = 'http://yourserver/zabbix/'
-zabbixUser      = 'someuser'
-zabbixPass      = 'somepass'
+# Class for argparse env variable support
+class EnvDefault(argparse.Action):
+    # From https://stackoverflow.com/questions/10551117/
+    def __init__(self, envvar, required=True, default=None, **kwargs):
+        if not default and envvar:
+            if envvar in os.environ:
+                default = os.environ[envvar]
+        if required and default:
+            required = False
+        super(EnvDefault, self).__init__(default=default, required=required,
+                                         **kwargs)
 
+    def __call__(self, parser, namespace, values, option_string=None):
+        setattr(namespace, self.dest, values)
+
+
+def jsonPrint(jsonUgly):
+    print(json.dumps(jsonUgly, indent=4, separators=(',', ': ')))
+
+
+def ArgumentParser():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-Z',
+                        required=True,
+                        action=EnvDefault,
+                        envvar='ZABBIX_SERVER',
+                        help="Specify the zabbix server URL ie: http://yourserver/zabbix/ (ZABBIX_SERVER environment variable)",
+                        metavar='zabbix-server-url')
+
+    parser.add_argument('-u',
+                        required=True,
+                        action=EnvDefault,
+                        envvar='ZABBIX_USERNAME',
+                        help="Specify the zabbix username (ZABBIX_USERNAME environment variable)",
+                        metavar='Username')
+
+    parser.add_argument('-p',
+                        required=True,
+                        action=EnvDefault,
+                        envvar='ZABBIX_PASSWORD',
+                        help="Specify the zabbix username (ZABBIX_PASSWORD environment variable)",
+                        metavar='Password')
+
+    parser.add_argument('-f',
+                        required=True,
+                        help="Specify the \"from\" date - format 26/6/2018 00:00",
+                        metavar='from-date')
+
+    parser.add_argument('-t',
+                        required=True,
+                        help="Specify the \"to\" date - format 26/6/2018 00:00",
+                        metavar='to-date')
+
+    parser.add_argument('-H',
+                        required=True,
+                        help="Specify the hostname of the monitored device",
+                        metavar='hostname')
+
+    parser.add_argument('-I',
+                        required=True,
+                        help="Specify the item name",
+                        metavar='to-date')
+
+    return parser.parse_args()
 
 
 def main(argv):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-H', required=True, metavar='Hostname')
-    parser.add_argument('-I', required=True, metavar='Item Name')
-    parser.add_argument('-f', required=True, metavar='From Timestamp')
-    parser.add_argument('-t', required=True, metavar='Till Timestamp')
 
-    args = parser.parse_args()
+    # Parse arguments and build work variables
+    args = ArgumentParser()
+    zabbixURL = args.Z
+    zabbixUsername = args.u
+    zabbixPassword = args.p
+    itemName = args.I
+    hostName = args.H
 
+    hostFilter = {'name': hostName}
+    itemFilter = {'name': itemName}
 
-    zapi = ZabbixAPI(url=zabbixServer, user=zabbixUser, password=zabbixPass)
+    # Convert from/to dates into timestamps
+    fromTimestamp = time.mktime(datetime.datetime.strptime(
+        args.f, "%d/%m/%Y %H:%M").timetuple())
+    tillTimestamp = time.mktime(datetime.datetime.strptime(
+        args.t, "%d/%m/%Y %H:%M").timetuple())
 
-    fromTimestamp = time.mktime(datetime.datetime.strptime(args.f, "%d/%m/%Y %H:%M").timetuple())
-    tillTimestamp = time.mktime(datetime.datetime.strptime(args.t, "%d/%m/%Y %H:%M").timetuple())
+    # API Connect
+    print('Connecting to {}'.format(zabbixURL))
+    try:
+        zapi = ZabbixAPI(url=zabbixURL, user=zabbixUsername,
+                         password=zabbixPassword)
+    except Exception as e:
+        print("Connection error: {}".format(str(e)))
+        exit()
 
+    # Search the hostname
+    try:
+        hosts = zapi.host.get(filter=hostFilter, output=[
+                              'hostids', 'host', 'name'])
+    except Exception as e:
+        print("Host get error:{}".format(str(e)))
+        exit()
 
-    f  = {  'name' : args.I  }
-    items = zapi.item.get(filter=f, host=args.H, output='extend' )
+    if (len(hosts) == 0):
+        print("Can't find host {}".format(hostName))
+        exit()
 
+    # Search the item object
+    try:
+        items = zapi.item.get(filter=itemFilter, host=hosts[0]['host'],
+                              output='extend', selectHosts=['host', 'name'])
+    except Exception as e:
+        print("Get item error: {}".format(str(e)))
+        exit()
+
+    print("Host {} ({}):".format(hosts[0]['name'], hosts[0]['host']))
+    # for loop - future fuzzy search
     for item in items:
-        print "ItemID: {} - Item: {} - Key: {}".format(item['itemid'], item['name'], item['key_'])
-        
-        values = zapi.trend.get(itemids=item['itemid'], time_from=fromTimestamp, time_till=tillTimestamp, history=item['value_type'])
+        print(
+            "ItemID: {} - Item: {} - Key: {}".format(item['itemid'], item['name'], item['key_']))
 
-        for historyValue in values:
-            currentDate = datetime.datetime.fromtimestamp(int(historyValue['clock'])).strftime('%d/%m/%Y %H:%M:%S')
+        # Search values
+        try:
+            values = zapi.trend.get(
+                itemids=item['itemid'], time_from=fromTimestamp, time_till=tillTimestamp, history=item['value_type'])
+        except Exception as e:
+            print("Values get error: {}".format(str(e)))
+            continue
 
-            print "{} {} Value: Min: {} - Max: {} - Avg: {}".format(historyValue['clock'], currentDate, historyValue['value_min'], historyValue['value_max'], historyValue['value_avg'],)
+        # Convert date/timestamp and print values
+        for trendValue in values:
+            currentDate = datetime.datetime.fromtimestamp(
+                int(trendValue['clock'])).strftime('%d/%m/%Y %H:%M:%S')
+
+            print("{} {} Value: Min: {} - Max: {} - Avg: {}".format(
+                trendValue['clock'], currentDate, trendValue['value_min'], trendValue['value_max'], trendValue['value_avg'],))
+
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
-
+    main(sys.argv[1:])
