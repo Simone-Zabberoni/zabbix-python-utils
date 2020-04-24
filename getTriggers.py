e5755ddcd25b08ce3fda6a4c035ffe66649c6173
Simone Zabberoni <simone.zabberoni@gmail.com>
2020-04-24 12:37:51 +0200

    get trigger and set trigger status

---
+++ getTriggers.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Search triggers by name, print the id and name (or extended info with -e)
+
+
+"""
+
+
+from zabbix.api import ZabbixAPI
+import logging
+import json
+import csv
+import argparse
+import getpass
+import getopt
+import re
+import sys
+import os
+
+
+# Class for argparse env variable support
+
+
+class EnvDefault(argparse.Action):
+    # From https://stackoverflow.com/questions/10551117/
+    def __init__(self, envvar, required=True, default=None, **kwargs):
+        if not default and envvar:
+            if envvar in os.environ:
+                default = os.environ[envvar]
+        if required and default:
+            required = False
+        super(EnvDefault, self).__init__(default=default, required=required,
+                                         **kwargs)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        setattr(namespace, self.dest, values)
+
+
+def jsonPrint(jsonUgly):
+    print(json.dumps(jsonUgly, indent=4, separators=(',', ': ')))
+
+
+def ArgumentParser():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-Z',
+                        required=True,
+                        action=EnvDefault,
+                        envvar='ZABBIX_SERVER',
+                        help="Specify the zabbix server URL ie: http://yourserver/zabbix/ (ZABBIX_SERVER environment variable)",
+                        metavar='zabbix-server-url')
+
+    parser.add_argument('-u',
+                        required=True,
+                        action=EnvDefault,
+                        envvar='ZABBIX_USERNAME',
+                        help="Specify the zabbix username (ZABBIX_USERNAME environment variable)",
+                        metavar='Username')
+
+    parser.add_argument('-p',
+                        required=True,
+                        action=EnvDefault,
+                        envvar='ZABBIX_PASSWORD',
+                        help="Specify the zabbix username (ZABBIX_PASSWORD environment variable)",
+                        metavar='Password')
+
+    parser.add_argument('-t',
+                        required=True,
+                        help="Trigger to search",
+                        metavar='trigger')
+
+    parser.add_argument('-e', action='store_true',
+                        help="Extended output")
+
+    return parser.parse_args()
+
+
+def main(argv):
+
+    # Parse arguments and build work variables
+    args = ArgumentParser()
+    zabbixURL = args.Z
+    zabbixUsername = args.u
+    zabbixPassword = args.p
+    triggerNameFilter = args.t
+
+    zapi = ZabbixAPI(url=zabbixURL, user=zabbixUsername,
+                     password=zabbixPassword)
+
+    f = {'description': triggerNameFilter}
+    triggers = zapi.trigger.get(search=f, output='extend', selectTags='extend')
+
+    if (args.e):
+        jsonPrint(triggers)
+    else:
+        for trigger in triggers:
+            print(
+                "ID: {} - Status: {} - Trigger: {}".format(trigger['triggerid'], trigger['status'], trigger['description']))
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ setTriggerStatus.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Enable or disable by triggerid
+
+"""
+
+
+from zabbix.api import ZabbixAPI
+import logging
+import json
+import csv
+import argparse
+import getpass
+import getopt
+import re
+import sys
+import os
+
+
+# Class for argparse env variable support
+
+
+class EnvDefault(argparse.Action):
+    # From https://stackoverflow.com/questions/10551117/
+    def __init__(self, envvar, required=True, default=None, **kwargs):
+        if not default and envvar:
+            if envvar in os.environ:
+                default = os.environ[envvar]
+        if required and default:
+            required = False
+        super(EnvDefault, self).__init__(default=default, required=required,
+                                         **kwargs)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        setattr(namespace, self.dest, values)
+
+
+def jsonPrint(jsonUgly):
+    print(json.dumps(jsonUgly, indent=4, separators=(',', ': ')))
+
+
+def ArgumentParser():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-Z',
+                        required=True,
+                        action=EnvDefault,
+                        envvar='ZABBIX_SERVER',
+                        help="Specify the zabbix server URL ie: http://yourserver/zabbix/ (ZABBIX_SERVER environment variable)",
+                        metavar='zabbix-server-url')
+
+    parser.add_argument('-u',
+                        required=True,
+                        action=EnvDefault,
+                        envvar='ZABBIX_USERNAME',
+                        help="Specify the zabbix username (ZABBIX_USERNAME environment variable)",
+                        metavar='Username')
+
+    parser.add_argument('-p',
+                        required=True,
+                        action=EnvDefault,
+                        envvar='ZABBIX_PASSWORD',
+                        help="Specify the zabbix username (ZABBIX_PASSWORD environment variable)",
+                        metavar='Password')
+
+    parser.add_argument('-i',
+                        required=True,
+                        help="Trigger ID",
+                        metavar='triggerid')
+
+    parser.add_argument('-s',
+                        required=True,
+                        help="Status (0=on, 1=off)",
+                        metavar='triggerstatus')
+
+    return parser.parse_args()
+
+
+def main(argv):
+
+    # Parse arguments and build work variables
+    args = ArgumentParser()
+    zabbixURL = args.Z
+    zabbixUsername = args.u
+    zabbixPassword = args.p
+
+    zapi = ZabbixAPI(url=zabbixURL, user=zabbixUsername,
+                     password=zabbixPassword)
+
+    triggers = zapi.trigger.update(triggerid=args.i, status=args.s)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
